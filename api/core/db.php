5870dccebc93247facf279c1f02be74f9ff197d8
Les <lester@gmail.com>
2026-07-15 15:44:41 +0800

    fix backend db

--- api/core/db.php
+++ api/core/db.php
@@ -15,7 +15,8 @@ try {
     $pdo = new PDO($dsn, $config['db']['user'], $config['db']['pass'], [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_general_ci"
     ]);
 } catch (PDOException $e) {
     http_response_code(500);
